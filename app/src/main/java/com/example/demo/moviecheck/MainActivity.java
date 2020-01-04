8329d53299c874e672fc87ff590369a7cde98be1
amandesai01 <amandesai01@gmail.com>
2020-01-04 14:30:47 +0530

    Binding Done

--- app/src/main/java/com/example/demo/moviecheck/MainActivity.java
+++ app/src/main/java/com/example/demo/moviecheck/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.demo.moviecheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
