62a989d3159777c45c306c0146606dd1c2b740bf
amandesai01 <amandesai01@gmail.com>
2020-01-04 14:53:12 +0530

    Api Call Done

--- app/src/main/java/com/example/demo/moviecheck/MainActivity.java
+++ app/src/main/java/com/example/demo/moviecheck/MainActivity.java
@@ -8,6 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button;
@@ -37,12 +49,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        class MyTask extends AsyncTask<String, Void, String>{
+    }
+    class MyTask extends AsyncTask<String, Void, String>{
+        String searchResult = "";
+        String jsonString = "";
+        String line = "";
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            try {
+                url = new URL(strings[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.connect();
+                InputStream inputStreamReader = con.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStreamReader));
+                while((line = reader.readLine()) != null){
+                    jsonString += line + "\n";
+                }
+                if(jsonString != null){
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    JSONArray jsonArray = jsonObject.getJSONArray("Search");
+                    for(int i = 0; i < jsonArray.length() ; i++){
+                        JSONObject movie = jsonArray.getJSONObject(i);
+                        String title = movie.getString("Title");
+                        String year = movie.getString("Year");
+                        searchResult += title + " " + year + "\n";
+                    }
+                }
 
-            @Override
-            protected String doInBackground(String... strings) {
-
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+            return searchResult;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(searchResult);
         }
     }
 }
